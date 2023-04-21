7213fb32afc72cd457127844bccc634fa1163c81
Soronprfbs <dople1227@gmail.com>
2023-04-22 06:21:54 +0900

    첫번째 커밋 commiteed

--- Survey/backend/myproject/settings.py
+++ Survey/backend/myproject/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -105,11 +103,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'djangodb',
-
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.mysql',       
     }
 }
 
