c788d6c545385f79fb391614a532d1257ba8ed21
Soronprfbs <dople1227@gmail.com>
2023-04-25 23:54:49 +0900

    aws

--- Survey/backend/myproject/settings.py
+++ Survey/backend/myproject/settings.py
@@ -68,17 +68,27 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://ap-northeast-2.compute.amazonaws.com",
-    "http://ec2-43-200-172-153.ap-northeast-2.compute.amazonaws.com",
-    "https://ap-northeast-2.compute.amazonaws.com",
+    "http://ap-northeast-2.compute.amazonaws.com:8000",
+    "http://ec2-43-200-172-153.ap-northeast-2.compute.amazonaws.com:8000",
 ]
 
 # Cors 에러 방지용 (꼭 필요한지 테스트 필요)
 CORS_ALLOW_CREDENTIALS = True
 # CSRF 토큰 헤더 설정
 CSRF_COOKIE_NAME = "csrftoken"
-CSRF_HEADER_NAME = "X-CSRFToken"
+# CSRF_HEADER_NAME = "X-CSRFToken"
 
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 ROOT_URLCONF = "myproject.urls"
 
