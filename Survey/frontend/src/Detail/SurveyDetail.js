0b1636afc409d1ab56bb689f66559af2cf7623dd
Soronprfbs <dople1227@gmail.com>
2023-04-26 11:09:36 +0900

    AWS

--- Survey/frontend/src/Detail/SurveyDetail.js
+++ Survey/frontend/src/Detail/SurveyDetail.js
@@ -9,29 +9,30 @@ function SurveyDetail() {
   const [error, setError] = useState(null);
   const { id } = useParams();
   const [localStateDetail, setLocalStateDetail] = useState();
-
+  return <div>Detail {id}</div>;
   // url 파라미터값 변경 시 실행
-  useEffect(() => {
-    const fetchData = async () => {
-      if (id) {
-        setLoading(true);
-        const url = `http://ec2-15-164-163-67.ap-northeast-2.compute.amazonaws.com:8000/api/detail/${id}/`;
-        // const url = `http://localhost:8000/api/detail/${id}/`;
-        try {
-          // const response = await axios.get(url);
-          // const { surveyName, questions } = response.data;
-          // console.log(response.data);
-          // setLocalStateDetail(questions);
-        } catch (e) {
-          setError(e);
-        } finally {
-          setLoading(false);
-        }
-      } else {
-        return <div>Detail {id}</div>;
-      }
-    };
-    fetchData();
-  }, [id]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     if (id) {
+  //       setLoading(true);
+  //       // const url = `http://ec2-15-164-163-67.ap-northeast-2.compute.amazonaws.com:8000/api/detail/${id}/`;
+  //       // const url = `http://localhost:8000/api/detail/${id}/`;
+  //       try {
+  //         // const response = await axios.get(url);
+  //         // const { surveyName, questions } = response.data;
+  //         // console.log(response.data);
+  //         // setLocalStateDetail(questions);
+  //         return <div>Detail {id}</div>;
+  //       } catch (e) {
+  //         setError(e);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     } else {
+  //       return <div>Detail {id}</div>;
+  //     }
+  //   };
+  //   fetchData();
+  // }, [id]);
 }
 export default SurveyDetail;
