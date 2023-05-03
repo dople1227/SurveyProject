09206413ccc2d4f9ff2249f35ebcfe70c0211cee
Soronprfbs <dople1227@gmail.com>
2023-05-03 13:06:29 +0900

    gitignore cash refresh

--- Survey/frontend/src/exception/PageNotFound.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageNotFound = () => {
-  return <h2>404 - Page not found</h2>;
-};
-
-export default PageNotFound;
--- Survey/frontend/src/soron/Soron.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
-function Soron() {
-  return (
-    <div>HELLO!</div>
-  )
-}
-export default Soron;
