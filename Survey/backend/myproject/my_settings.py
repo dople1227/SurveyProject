9c70826d084a6dc12dff4a803ca510d531eb20fc
Soronprfbs <dople1227@gmail.com>
2023-05-03 11:31:55 +0900

    gitignore refresh

---
+++ Survey/backend/myproject/my_settings.py
@@ -0,0 +1,13 @@
+MY_SECRET = {"SECRET_KEY": "django-insecure-!5)#(f^z^p&z%6zz4649l8au7k8eb31lc-c_6j^cu*slzzs%wu"}
+
+MY_DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "djangodb",
+        "USER": "AlohaAdmin",
+        "PASSWORD": "aloha123!@#",
+        "HOST": "43.201.114.17",
+        # "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
